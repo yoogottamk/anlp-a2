c16d7cc74ef8b09a686d27a813172d963785e4bf
yoogottamk <yoogottamk@outlook.com>
2021-11-09 21:55:47 +0530

    improved inference code, added sample inputs

--- anlp_a2/inference.py
+++ anlp_a2/inference.py
@@ -1,3 +1,5 @@
+from itertools import combinations
+
 import numpy as np
 import torch
 from allennlp_models.lm.modules.token_embedders.bidirectional_lm import (
@@ -7,11 +9,13 @@ from allennlp.data.token_indexers.elmo_indexer import (
     ELMoTokenCharactersIndexer as ETCIndexer,
 )
 from allennlp.data.tokenizers.token_class import Token
+from scipy.spatial.distance import cosine, euclidean
 
 from anlp_a2.config import DATASET_DIR
 
 model_file = str(DATASET_DIR.parent / "model" / "model.tar.gz")
 ENCODER = BLMTEncoder(archive_file=model_file, bos_eos_tokens=None)
+ENCODER.eval()
 INDEXER = ETCIndexer()
 vocab = ENCODER._lm.vocab
 
@@ -23,11 +27,31 @@ def get_embeddings(sentence: str, word: str):
     character_indices = INDEXER.tokens_to_indices(tokens, vocab)["elmo_tokens"]
     indices_tensor = torch.LongTensor([character_indices])
 
-    emb = ENCODER(indices_tensor).numpy()
+    emb = ENCODER(indices_tensor).detach().numpy()
     return emb[0][idx]
 
-
 if __name__ == "__main__":
-    print(get_embeddings("i am wearing a big ring", "ring").shape)
-    print(get_embeddings("i have a diamond ring", "ring").shape)
-    print(get_embeddings("my phone rings", "rings").shape)
+    N = int(input("#sentences: "))
+    words = []
+    sentences = []
+    embeddings = []
+
+    idx_list = list(combinations(range(N), 2))
+
+    for _ in range(N):
+        sentence = input("Enter sentence: ").strip()
+        word = input("Enter word: ").strip()
+        sentences.append(sentence)
+        words.append(word)
+        embeddings.append(get_embeddings(sentence, word))
+
+    print()
+    for w1, w2 in idx_list:
+        print("===" * 20)
+        print(sentences[w1])
+        print(sentences[w2])
+        print("---" * 20)
+        print(f"{words[w1]}[{w1}] <-> {words[w2]}[{w2}]")
+        print(f"Euclidean Distance: {euclidean(embeddings[w1], embeddings[w2])}")
+        print(f"   Cosine Distance: {cosine(embeddings[w1], embeddings[w2])}")
+        print("===" * 20)
